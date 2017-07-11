03a0390591d868bbac03e138a807e73a89b06c8d
David Běhal <behal.david@gmail.com>
2017-07-11 19:51:37 +0200

    First commit

---
+++ src/Providers/AdminAuthProvider.php
@@ -0,0 +1,35 @@
+<?php namespace Brackets\Admin;
+
+use Brackets\AdminGenerator\Generate\ModelFactory;
+use Illuminate\Support\ServiceProvider;
+
+class AdminProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'brackets/admin');
+
+        $this->publishes([
+            __DIR__.'/../install-stubs/resources/assets' => resource_path('assets')
+        ], 'assets');
+
+        $this->publishes([
+            __DIR__.'/../install-stubs/resources/views' => resource_path('views')
+        ], 'views');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
