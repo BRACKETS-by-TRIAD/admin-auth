98e847fbc0568b10b28b918eaa00a2b6e67cdf8e
David Běhal <behal.david@gmail.com>
2017-09-21 09:41:28 +0200

    Refactored trans keys

--- resources/lang/sk/admin.php
+++ resources/lang/sk/admin.php
@@ -1,32 +1,32 @@
 <?php
 
 return [
-    'auth-global' => [
+    'auth_global' => [
         'email' => 'E-Mail',
         'password' => 'Heslo',
-        'password-confirm' => 'Potvrdenie hesla',
+        'password_confirm' => 'Potvrdenie hesla',
     ],
 
     'login' => [
         'title' => 'Prihlásenie',
-        'signInText' => 'Prihlásenie do účtu',
+        'sign_in_text' => 'Prihlásenie do účtu',
         'button' => 'Prihlásenie',
-        'forgotPassword' => 'Zabudli ste heslo?',
+        'forgot_password' => 'Zabudli ste heslo?',
     ],
 
-    'password-reset' => [
+    'password_reset' => [
         'title' => 'Obnova hesla',
         'note' => 'Obnoviť zabudnuté heslo',
         'button' => 'Obnoviť heslo',
     ],
 
-    'forgot-password' => [
+    'forgot_password' => [
         'title' => 'Obnova hesla',
         'note' => 'Odoslať e-mail na obnovu hesla',
         'button' => 'Odoslať link na obnovu hesla',
     ],
 
-    'activation-form' => [
+    'activation_form' => [
         'title' => 'Aktivácia účtu',
         'note' => 'Odoslať aktivačný link na e-mail',
         'button' => 'Odoslať aktivačný link',
@@ -35,15 +35,15 @@ return [
     'activations' => [
         'sent' => 'Poslali sme Vám aktivačný link.',
         'activated' => 'Váš účet bol aktitovaný.',
-        'invalid-request' => 'Požiadavka bola zamietnutá.',
+        'invalid_request' => 'Požiadavka bola zamietnutá.',
         'disabled' => "Aktivácia nebola povolená.",
     ],
 
     'passwords' => [
         'reset' => 'Vaše heslo bolo obnovené.',
         'sent' => 'Poslali sme Vám link na obnovu hesla.',
-        'invalid-password' => 'Heslo musí obsahovať najmenej šesť znakov a musí sa zhodovať s potvrdením.',
-        'invalid-token' => 'Token na obnovu hesla je neplatný.',
-        'invalid-user' => "Nepodarilo sa nájsť používateľa s touto e-mailovou adresou.",
+        'invalid_password' => 'Heslo musí obsahovať najmenej šesť znakov a musí sa zhodovať s potvrdením.',
+        'invalid_token' => 'Token na obnovu hesla je neplatný.',
+        'invalid_user' => "Nepodarilo sa nájsť používateľa s touto e-mailovou adresou.",
     ]
 ];
