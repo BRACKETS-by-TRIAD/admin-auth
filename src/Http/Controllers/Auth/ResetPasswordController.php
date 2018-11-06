bff220e7fac2cfecbaf0765ce2af6a4f8f85629b
Pavol Perdík <pavol.perdik@brackets.sk>
2018-11-06 11:29:40 +0100

    Make upgrade to Laravel 5.7 backward compatible (#9)
    * wip
    
    * wip
    
    * wip

--- src/Http/Controllers/Auth/ResetPasswordController.php
+++ src/Http/Controllers/Auth/ResetPasswordController.php
@@ -78,6 +78,34 @@ class ResetPasswordController extends Controller
     }
 
     /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function reset(Request $request)
+    {
+        $this->validate($request, $this->rules(), $this->validationErrorMessages());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+            $this->resetPassword($user, $password);
+        }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+            ? $this->sendResetResponse($request, $response)
+            : $this->sendResetFailedResponse($request, $response);
+    }
+
+    /**
      * Get the response for a successful password reset.
      *
      * @param Request $request
