2198cdfcf9628609aeb29743ee65199f401c3a45
David Běhal <behal.david@gmail.com>
2018-11-12 10:55:45 +0100

    Some adjustments

--- src/Listeners/ActivationListener.php
+++ src/Listeners/ActivationListener.php
@@ -16,21 +16,35 @@ class ActivationListener
     protected $activationBroker = 'admin_users';
 
     /**
+     * Create a new ActivationListener
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->activationBroker = config('admin-auth.defaults.activations');
+    }
+
+    /**
      * Register the listeners for the subscriber.
      *
      * @param Illuminate\Events\Dispatcher|Dispatcher $events
      */
     public function subscribe(Dispatcher $events)
     {
-        $userClass = Activation::broker($this->activationBroker)->getUserModelClass();
-        $interfaces = class_implements($userClass);
-        if (($interfaces && in_array(\Brackets\AdminAuth\Contracts\Auth\CanActivate::class, $interfaces))) {
-            $events->listen(
-                'eloquent.created: ' . $userClass,
-                ActivationService::class
-            );
+        $activatinBrokerConfig = config("activation.activations.{$this->activationBroker}");
+        if(!is_null(app('auth')->createUserProvider($activatinBrokerConfig['provider']))) {
+            $userClass = Activation::broker($this->activationBroker)->getUserModelClass();
+            $interfaces = class_implements($userClass);
+            if ($interfaces && in_array(\Brackets\AdminAuth\Contracts\Auth\CanActivate::class, $interfaces)) {
+                $events->listen(
+                    'eloquent.created: ' . $userClass,
+                    ActivationService::class
+                );
+            }
+
+            //TODO listen on user edit and if email has changed, deactivate user and send email again
         }
 
-        //TODO listen on user edit and if email has changed, deactivate user and send email again
     }
 }
