b33c20cd59617190a0f7855a9bf74f77bda22598
Pavol Perdík <palypster@gmail.com>
2020-11-03 16:28:31 +0100

    Fix: change in namespaces

--- src/Models/AdminUser.php
+++ src/Models/AdminUser.php
@@ -14,8 +14,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Image\Exceptions\InvalidManipulation;
-use Spatie\MediaLibrary\HasMedia\HasMedia;
-use Spatie\MediaLibrary\Models\Media;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Permission\Traits\HasRoles;
 
 /**
@@ -106,7 +106,7 @@ class AdminUser extends Authenticatable implements CanActivateContract, HasMedia
     /**
      * Register media collections
      */
-    public function registerMediaCollections()
+    public function registerMediaCollections(): void
     {
         $this->addMediaCollection('avatar')
             ->accepts('image/*');
@@ -118,7 +118,7 @@ class AdminUser extends Authenticatable implements CanActivateContract, HasMedia
      * @param Media|null $media
      * @throws InvalidManipulation
      */
-    public function registerMediaConversions(Media $media = null)
+    public function registerMediaConversions(Media $media = null): void
     {
         $this->autoRegisterThumb200();
 
